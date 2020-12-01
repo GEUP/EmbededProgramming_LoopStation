6a226150e81f146918ef7153384056a47dee3e9d
Eun Ran <33981028+GEUP@users.noreply.github.com>
2020-12-01 10:18:49 +0900

    LoopStation.py

--- LoopStation.py
+++ LoopStation.py
@@ -1,11 +1,22 @@
 import paho.mqtt.client as mqtt
 import time
 
-mqtt = mqtt.Client("LoopStation")
-mqtt.connect("localhost",1883)
+def on_connect(client,userdata,flags,rc):
+    client.subscribe("SOURCE")
+    
+def on_message(client,userdata,msg):
+    print("message arrive")
+    mqttClient.publish("CTRL-SPEAKER",msg,payload)
+    f=open('source.3gpp','w')
+    #f.write(msg.payload)
+    f.close()
 
+mqttClient = mqtt.Client("LoopStation")
+mqttClient.on_connect=on_connect
+mqttClient.on_message = on_message
+mqttClient.connect("localhost",1883)
+
+mqttClient.loop_start()
 while True:
-	mqtt.publish("CTRL-MIKE","MIKE ON")
-	time.sleep(5)
-	mqtt.publish("CTRL-MIKE","MIKE OFF")
-	time.sleep(5)
+    mqttClient.publish("CTRL-MIKE","MIKE ON")
+    time.sleep(10)
