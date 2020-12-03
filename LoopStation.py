a59d285e9172e2803f950849ab400677f5d0567e
GEUP <knmj789@gmail.com>
2020-12-03 10:42:05 +0900

    1

--- LoopStation.py
+++ LoopStation.py
@@ -2,7 +2,7 @@ import paho.mqtt.client as mqtt
 import time
 from pydub import AudioSegment
 
-song = AudioSegment.from_file('/home/pi/Desktop/source.3gp',formet='arm')
+song = AudioSegment.from_file('/home/pi/Desktop/source.3gp')
 song = song-100
 num=0
 def on_connect(client,userdata,flags,rc):
@@ -16,15 +16,16 @@ def on_message(client,userdata,msg):
     fw=open('temp.3gp','wb')
     #fw=open('source.3gp','wb')
     fw.write(msg.payload)
+    fw.close()
+    
     data = AudioSegment.from_file('/home/pi/Desktop/temp.3gp',formet='arm')
-    song  = song.overlay(data)
-    print(type(song))
+    song  = song.overlay(data*2)
     song.export('/home/pi/Desktop/source.3gp')
     fr=open('source.3gp','rb')
     sendData = fr.read()
-    mqttClient.publish("CTRL-SPEAKER",sendData)
     fr.close()
-    fw.close()
+    mqttClient.publish("CTRL-SPEAKER",sendData)
+    #mqttClient.publish("CTRL-SPEAKER",msg.payload)
 
 mqttClient = mqtt.Client("LoopStation")
 mqttClient.on_connect=on_connect
@@ -32,11 +33,11 @@ mqttClient.on_message = on_message
 mqttClient.connect("localhost",1883)
 
 mqttClient.loop_start()
+
 while True:
-	print("MIKE ON")
-	mqttClient.publish("CTRL-MIKE","MIKE ON")
-	time.sleep(5)
-	print("MIKE OFF")
-	mqttClient.publish("CTRL-MIKE","MIKE OFF")
-	time.sleep(2)
-	
+    print("MIKE ON")
+    mqttClient.publish("CTRL-MIKE","MIKE ON")
+    time.sleep(20)
+    print("MIKE OFF")
+    mqttClient.publish("CTRL-MIKE","MIKE OFF")
+    time.sleep(62)
